8b4ae3cef20ce304cc788c255739b85b0cade0ba
chipmunkcol <jack95711@gmail.com>
2022-07-31 11:33:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
+/*eslint-disable */
 import './App.css';
+import { Col, Row, Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, useNavigate, Link, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import jsonData from './data';
 
 function App() {
+// let navigate = useNavigate();
+
+let[jsonData1] = useState(jsonData)
+console.log(jsonData1)
+
+useEffect(()=>{
+  
+},[]) 
+
+function json() {
+   axios.get('')
+   .then((response)=>{
+   console.log(response.data)
+   })
+  .catch(()=>{
+   alert('ajax 데이터 잘못된거같은디?');
+ })
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello, react</h1>
+  <BrowserRouter>
+     <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>Coffee</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href='/cart'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    <Routes>
+      
+           <Route path='/' element={<Main jsonData1={jsonData1}/>} />
+
+           <Route path={'/detail/:id'} element={<div>메롱메롱</div>} />
+
+           <Route path='/cart' element={<div>카트입니다</div>} />
+
+    </Routes>
+      
+  </BrowserRouter>
     </div>
+  );
+}
+
+
+function Main({jsonData1}){
+
+  
+
+  return(
+    <>
+
+    <h1>기러기역삼역토마토 홈쇼핑</h1>
+    <img style={{width:'40%'}} src={require('./img/80179016571916020.jpg')} />
+    <p>어머, 이건 사야해!</p>
+
+
+    <Container> 
+      <Row md={3}>
+        {
+          jsonData1.map((val,i)=>
+            <Col key={i} style={{display:"flex"}} onClick={()=>{
+              Navigate()
+            }}>
+              <div className={'img0'+i}></div>
+              <div className='img_text'>
+              <h4>{val.title}</h4>
+              <h6>{val.price}원</h6>
+              <p>{val.content}</p>
+              </div>
+            </Col>
+          )
+        }
+        
+      </Row>
+    </Container>
+
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,7 @@
+const jsonData = [
+{id: 3, title: '우영우김밥', content: '우영우의 주식 맛있다', price: 3000},
+{id: 4, title: '우영우김초밥', content: '고오급 김초밥', price: 10000},
+{id: 5, title: '흑동고래', content: '우영우의 고래', price: 1000000}
+]
+
+export default jsonData;
